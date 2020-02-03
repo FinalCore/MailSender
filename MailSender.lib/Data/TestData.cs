523aa6800bc349a6dd4380ba3728c1d998bfcc18
FinalCore <rimedheart@yandex.ru>
2020-02-03 22:00:08 +0300

    Добавил тестовые данные

---
+++ MailSender.lib/Data/TestData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.lib.Entities;
+
+namespace MailSender.lib.Data
+{
+    public static class TestData
+    {
+        public static List<Server> Servers { get; } = new List<Server>
+        {
+            new Server("Яндекс", "smtp.yandex.ru", 587, "UserLogin", "Password"),
+            new Server("Яндекс", "smtp.mail.ru", 587, "UserLogin", "Password"),
+            new Server("Яндекс", "smtp.gmail.com", 587, "UserLogin", "Password")
+        };
+
+        public static List<Sender> Senders { get; } = new List<Sender>
+        {
+            new Sender("Пупкин", "v.pupkin@mail.ru"),
+            new Sender("Петросян", "e.petrosyan@gamil.com"),
+            new Sender("Хабибулин", "a.habibulin@yandex.ru")
+        };
+    }
+}
---
+++ MailSender.lib/Entities/Sender.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.lib.Entities
+{
+    /// <summary>
+    /// Класс, описывающий отправителя
+    /// </summary>
+    public class Sender
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Sender(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+}
