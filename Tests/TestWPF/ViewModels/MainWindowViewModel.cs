8269dfda0062c4d001a595e3c0e096f75e6bf24a
FinalCore <rimedheart@yandex.ru>
2020-02-05 22:21:25 +0300

    Начал реализовывать паттерн MVVM в тестовом проекте WPF

---
+++ Tests/TestWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.lib.MVVM;
+
+namespace TestWPF.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string title = "Новое окно!";
+
+        public string Title2
+        {
+            get => title;
+            set => Set(ref title, value, Title2);
+        }
+            
+            
+    }
+}
