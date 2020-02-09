1b9fffd2ef94d44a43f3b3577d5904f96d7265b8
FinalCore <rimedheart@yandex.ru>
2020-02-09 13:56:05 +0300

    Revert "Revert "Начал реализовывать паттерн MVVM в тестовом проекте WPF""
    This reverts commit f6784f2a386c6a5f8d2eb671d394ac483d97cc15.

---
+++ MailSender.lib/MVVM/ViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MailSender.lib.MVVM
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        // Объявляем событие
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Метод, вызывающий событие при изменении определенного свойства, имя которого передается в метод
+        /// </summary>
+        /// <param name="PropertyName"></param>
+        protected virtual void OnPropertyChanged (string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        /// <summary>
+        /// Метод, изменяющий значение поля при изменении соответствующего свойства и вызывающий событие OnPropertyChanged в этом случае 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <param name="value"></param>
+        /// <param name="PropertyName"></param>
+        /// <returns></returns>
+        protected virtual bool Set<T>(ref T field, T value, string PropertyName)
+        {
+            if (Equals(field, value)) return false;
+            else
+            {
+                field = value;
+                OnPropertyChanged(PropertyName);
+                return true;
+            }
+        }
+
+    }
+}
